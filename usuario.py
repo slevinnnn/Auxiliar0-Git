fa22492540087af2d1032e73bcbd4544f69f3c84
slevinnnn <danielbm3012@gmail.com>
2025-03-19 19:02:17 -0300

    se elimino el else de la funcion listar tareas

--- usuario.py
+++ usuario.py
@@ -8,12 +8,8 @@ class Usuario:
     def agregarTarea(self, tarea):
         self.tareas.append(tarea)
 
-
-    def listarTareas(self):
-        for tarea in self.tareas:
-            if tarea.estaLista():
-                print(f"[X] {tarea.obtenerNombre()}" )
-            else:
-                print(f"[ ] {tarea.obtenerNombre()}" )
-
-                #hola
+def listarTareas(self):
+   for tarea in self.tareas:
+       if tarea.estaLista():
+           print(f"La tarea {tarea.obtenerNombre()} está lista")
+           
